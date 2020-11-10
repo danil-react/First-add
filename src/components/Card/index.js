a25e4e8415449ada2f79da42df95d98bf00fd3a3
danil-react <da.kuharenko@justice-it.com>
2020-11-10 14:52:34 +0300

    remodel logic

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,35 +1,14 @@
-import React, { useState } from "react";
-
-import img1 from "../../assets/images/lampA.png";
-import img2 from "../../assets/images/lampB.png";
+import React from "react";
+import styles from "./styles.module.scss"
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Card from "@material-ui/core/Card";
-import {Modal} from "antd"
-import ModalCard from './ModalCard.js'
 
-import styles from "./styles.module.scss"
+const CardNew = ({products, onOpen}) => {
 
-const arr = [
-    {
-        img: img1,
-        title: "Blue Desk",
-        price: "$250.00",
-    },
-    {
-        img: img2,
-        title: "Gold Desk",
-        price: "$200.00",
-
-    },
-]
-
-const CardNew = () => {
-    const [productIndex, setModal] = useState(null)
-    const [isOpen, toggle] = useState(false);
     const useStyles = makeStyles({
         root: {
             maxWidth: 700,
@@ -39,24 +18,15 @@ const CardNew = () => {
         },
     });
 
-    const closeModal = () => {
-        setModal(null);
-        toggle(false)
-    }
-    const openModal = (index) => {
-        console.log(index)
-        setModal(index)
-        toggle(true)
-    }
     const classes = useStyles();
-    const selectedProduct = arr.find((item, i) => i === productIndex)
-console.log(selectedProduct)
+
     return (
         <div className={styles.card}>
-            {arr.map((item, index) => (
-                <Card className={classes.root} >
-                    <CardActionArea onClick={()=>{openModal(index)} }>
-
+            {products.map((item, index) => (
+                <Card className={classes.root}>
+                    <CardActionArea onClick={() => {
+                        onOpen(index)
+                    }}>
                         <CardMedia
                             className={styles.cardImage}
                             image={item.img}
@@ -73,10 +43,6 @@ console.log(selectedProduct)
                     </CardActionArea>
                 </Card>
             ))}
-            <Modal visible={isOpen} onOk={()=> console.log('function cart')} onCancel={closeModal}>
-                <ModalCard setClose={() => setModal(null)} selectedProduct={selectedProduct} />
-
-            </Modal>
         </div>
     );
 }
--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -1,22 +1,52 @@
-import React from "react";
-
-import Container from "@material-ui/core/Container";
-import CardNew from '../../components/Card'
+import React, {useState} from "react";
 import styles from "../../styles.module.scss";
-import Logo from "../../assets/images/lovelamp.svg";
+import Container from "@material-ui/core/Container";
+import {Modal} from "antd"
 
+import ModalCard from '../../components/ModalCard/ModalCard'
+import CardNew from '../../components/Card'
+
+import Logo from "../../assets/images/lovelamp.svg";
+import img1 from "../../assets/images/lampA.png";
+import img2 from "../../assets/images/lampB.png";
+
+const arr = [
+    {
+        img: img1,
+        title: "Blue Desk",
+        price: "$250.00",
+    },
+    {
+        img: img2,
+        title: "Gold Desk",
+        price: "$200.00",
+
+    },
+]
 const Home = () => {
-    const arr = ['a']
+    const [productIndex, setModal] = useState(null)
+    const [toggle, setToggle] = useState(false);
+
+    const closeModal = () => {
+        setModal(null);
+        setToggle(false)
+    }
+    const openModal = (index) => {
+        setModal(index)
+        setToggle(true)
+    }
+
     return (
         <Container>
             <div className={styles.logo}>
                 <img src={Logo} alt="logo"/>
             </div>
-            <div>
-                {arr.map((item) => (
-                    <CardNew name={item}/>
-                ))}
+            <div className={styles.img}>
+                <CardNew products={arr} onOpen={openModal}/>
             </div>
+            <Modal visible={toggle} onOk={() => console.log('fun')} onCancel={closeModal}>
+                <ModalCard setClose={() => setModal(null)} selectedProduct={arr[productIndex]}/>
+            </Modal>
         </Container>
     );
 }

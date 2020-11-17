aa10c83e3c82fd7c6c31cea653c73befc9dd94e6
danil-react <da.kuharenko@justice-it.com>
2020-11-17 17:40:52 +0300

    work on adaptivity/ complete ca

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,33 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 import {Navbar} from 'react-bootstrap'
 import {Nav} from 'react-bootstrap'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
-import styles from "./styles.module.scss";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
 import CartCount from "../CartCount"
 
-const NavbarNew = () => {
+const NavbarNew = ({numberOfCart}) => {
   return (
-
-    <Navbar bg="light" expand="lg">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className={styles.navbarUuuu}>
-        <div className={styles.nav}>
-          <Nav className={styles['custom-navbar']}>
-            <div className={styles.left}>
-              <div className={styles.block}>
-                <Link to="/">Starter Store</Link>
-              </div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <div className={styles.nav}>
+              <Nav >
+                <div className={styles.left}>
+                    <NavLink to="/" activeClassName={styles.active}><LoyaltyIcon />Starter Store</NavLink>
+                </div>
+                <div className={styles.right}>
+                    <NavLink to="/SignUp" className={styles.active}>Sign Up</NavLink>
+                    <NavLink to="/SignIn" className={styles.active}>Sign In</NavLink>
+                    <NavLink to="/Cart" className={styles.active}><ShoppingCartIcon />Cart(<CartCount numberOfCart={numberOfCart}/>)</NavLink>
+                </div>
+              </Nav>
             </div>
-            <div className={styles.right}>
-              <div className={styles.block}>
-                <Link to="/SignUp">SignUp</Link>
-              </div>
-              <div className={styles.block}>
-                <Link to="/SignIn">SignIn</Link>
-              </div>
-              <div className={styles.block}>
-                <Link to="/Cart">Cart{<CartCount />}</Link>
-              </div>
-            </div>
-          </Nav>
-        </div>
-      </Navbar.Collapse>
-    </Navbar>
+          </Navbar.Collapse>
+        </Navbar>
   )
 }
 
--- src/layouts/Home/index.js
+++ src/layouts/Home/index.js
@@ -28,23 +28,29 @@ const Home = ({setState}) => {
 
 
   return (
-    <Container>
-      <div className={styles.logo}>
-        <img src={Logo} alt="logo"/>
-      </div>
-      <div className={styles.img}>
-        <CardNew products={products} onOpen={openModal}/>
-      </div>
-      <Modal
-        className={styles.modal}
-        destroyOnClose
-        visible={toggle}
-        onOk={() => console.log('fun')}
-        onCancel={closeModal}
-      >
-        <ModalCard setClose={() => setModal(null)} selectedProduct={products[productIndex]} products={products} setState={setState}/>
-      </Modal>
-    </Container>
+    <div className={styles.container}>
+      <Container>
+        <div className={styles.logo}>
+          <img src={Logo} alt="logo"/>
+        </div>
+        <div className={styles.img}>
+          <CardNew products={products} onOpen={openModal}/>
+        </div>
+        <Modal
+          className={styles.modal}
+          destroyOnClose
+          visible={toggle}
+          onOk={() => console.log('fun')}
+          onCancel={closeModal}
+          width="60%"
+          footer={null}
+          wrapClassName="sa"
+        >
+          <ModalCard setClose={() => setModal(null)} selectedProduct={products[productIndex]} products={products} setState={setState}/>
+        </Modal>
+      </Container>
+    </div>
+
   );
 }
 export default Home
